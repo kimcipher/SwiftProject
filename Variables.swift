d4078f6428b560c4965ba5b09bc8abc8b3f1fd9b
kimcipher <kimutaij192@gmail.com>
2023-03-31 10:23:35 +0300

    constants

--- Variables.swift
+++ Variables.swift
@@ -34,3 +34,20 @@ var artist: String
 var book: String = "Just Kids"
 var pageCount: Int = 278
 
+// String Interpolation
+/* In Swift, to insert a value into a string using string interpolation, we can write the value in parentheses, and write a backslash \ before the parentheses. */
+// for example
+var albums = 17
+ 
+print("Daniel Johnston made \(albums) studio albums.")
+
+// Daniel Johnston made 17 studio albums.
+var age = 24
+print("I am \(age) years old.")
+
+
+// Constants
+
+/* Whereas variables can be reassigned during their lifetime, constants cannot. Once we assign a value to a constant in Swift, it becomes immutable. This means that the value stored in it cannot be changed */
+// constants are declared by let keyword
+let months = 4 // January, February, May, July
