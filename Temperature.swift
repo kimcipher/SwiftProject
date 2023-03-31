1768ee851a32bc218b299a287eaed3a584d5210c
kimcipher <kimutaij192@gmail.com>
2023-03-31 10:32:24 +0300

    converting Fahrenheit to Celsius

---
+++ Temperature.swift
@@ -0,0 +1,12 @@
+//The mad scientist Kelvin has mastered predicting the weather in his mountain-side meteorology lab.
+// Let’s convert a temperature from Fahrenheit (°F) to Celsius (°C).
+// The formula is the following:
+
+C=(F-32)/1.8
+
+var tempf:Double = 82.8
+var tempc:Double = 70.6
+
+tempc = (tempf - 32)/1.8
+print("The temp is \(tempc) degrees Celsius")
+// The temp is 28.222222222222 degrees Celsius
