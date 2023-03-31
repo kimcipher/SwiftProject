99240648c8d38fe523ec522ee89c6b1382358ee3
kimcipher <kimutaij192@gmail.com>
2023-03-31 09:51:47 +0300

    declaring

---
+++ Variables.swift
@@ -0,0 +1,14 @@
+// Introduction to variables
+
+/* A variable is simply a name that represents a particular piece of your computer’s memory that has been set aside for you to store, retrieve, and use data */
+
+// Basic data types
+
+// Int, Double, String, Bool
+
+/* Suppose we are building a game, and we want to keep track of the player’s name, the player’s score that goes from 0 to 10, the player’s level, etc. We need some variables! */
+
+var score = 0
+print(score)
+
+var year = 2023
