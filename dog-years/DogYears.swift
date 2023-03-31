f6ade5daf539236a22169fb311537ccb7a09fce1
kimcipher <kimutaij192@gmail.com>
2023-03-31 10:48:44 +0300

    DogYears x HumanYears

---
+++ dog-years/DogYears.swift
@@ -0,0 +1,13 @@
+// Dog years
+// SONNY LI
+
+var dogAge: Double = 2.5
+
+let earlyYears: Double = 21.0
+var laterYears: Double = 0.0
+var humanYears: Double = 0.0
+
+laterYears = (dogAge - 2) * 4
+humanYears = earlyYears + laterYears
+
+print("Ruff Ruff! My name is Landon and I'm a Boerbel. I'm \(humanYears) years old in human years.")
